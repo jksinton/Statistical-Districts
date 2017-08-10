483581991315dc3acf5b96d45b3094b1672defcd
James Sinton <jksinton@gmail.com>
2017-08-09 23:42:52 -0500

    Initial framework

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*
+ * This file is part of Statistical Districts.
+ * Copyright (c) 2017, James Sinton
+ * All rights reserved.
+ * Released under the BSD 3-Clause License
+ * https://github.com/jksinton/Statistical-Districts/blob/master/LICENSE
+ *
+ */
+
+$(function() {
+
+
+}
---
+++ statbuilder.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# This file is part of Statistical Districts.
+# 
+# Copyright (c) 2017, James Sinton
+# All rights reserved.
+# 
+# Released under the BSD 3-Clause License
+# https://github.com/jksinton/Statistical-Districts/blob/master/LICENSE
+# 
+
+import os
+import pandas as pd
+import geopandas as gpd
+from geopandas import GeoSeries, GeoDataFrame
+
