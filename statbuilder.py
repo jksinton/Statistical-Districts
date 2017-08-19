1c37234c0d99583a3838bc40bb993a4f3679438e
James Sinton <jksinton@gmail.com>
2017-08-19 01:05:01 -0500

    pid by age

--- statbuilder.py
+++ statbuilder.py
@@ -56,7 +56,7 @@ def get_command_line_args():
         Nothing (yet)
     """
     # TODO
-    # take state and district
+    # provide state, district, and year as options
     _version=VERSION
     parser = argparse.ArgumentParser(description='Build stats for a particular Congressional District')
     parser.add_argument('-v','--version',action='version', version='%(prog)s %(version)s' % {"prog": parser.prog, "version": _version})
@@ -68,9 +68,11 @@ def get_command_line_args():
 # thoughts on creating an object?
 # class CongDistrict(object):
 #    
-#    def __init__(self, state, district):
+#    def __init__(self, state=48, district=7, year=2015):
 #        self.state = state
 #        self.district = district
+#        self.year = year
+#
 
 # TODO
 # def find_tracts_in_district(state='48', district='07'):
@@ -81,8 +83,6 @@ def find_blockgroups_in_district(state='48', district='07'):
     Args:
         state: The state where the Congressional district is in
         district: Congressional district
-        blockgroup: Boolean
-        tract: Boolean
         plot: Boolean
     Returns:
         Nothing
@@ -93,12 +93,12 @@ def find_blockgroups_in_district(state='48', district='07'):
     # process based on state and district args
     # set based on args: state, distric
     bgs_output = 'district-blockgroups'
-    bgs_outputGeoJSON = 'geojson/' + bgs_output + '.geojson'
+    bgs_outputGeoJSON = 'static/geojson/' + bgs_output + '.geojson'
 
     # TODO 
     # see if geojson files is available for district
     #   if not generate geojson file for district
-    district_geojson = 'geojson/district.geojson'
+    district_geojson = 'static/geojson/district.geojson'
 
     # TODO download blockgroup shapefile for state, unzip, and read *.shp file
     census_block_groups_file = '2015_community_survey/tl_2015_48_bg/tl_2015_48_bg.shp' # Public domain ftp://ftp2.census.gov/geo/tiger/TIGER2015/BG/tl_2015_48_bg.zip
@@ -129,7 +129,7 @@ def find_blockgroups_in_district(state='48', district='07'):
     bgs_in_district[['BLKGRPCE','COUNTYFP', 'STATEFP', 'TRACTCE', 'GEOID']].to_csv('static/data/' + bgs_output +'.csv')
 
 
-def get_blockgroup_data(api, fields, year=2015, state='48', district='07'):
+def get_blockgroup_census_data(api, fields, state=48, district=7, year=2015):
     """Retrieve the census data for the block groups in a Congressional District
     Args:
         api: Census api key
@@ -148,7 +148,7 @@ def get_blockgroup_data(api, fields, year=2015, state='48', district='07'):
     
     # Setup Census query
     census_query = Census(api, year=year)
-    census_fields = []
+    census_fields = {}
     
     num_of_bgs = len(bgs_in_district)
     i = 0.0
@@ -165,15 +165,10 @@ def get_blockgroup_data(api, fields, year=2015, state='48', district='07'):
                         blockgroup=bg_row['BLKGRPCE'],
                         tract=bg_row['TRACTCE']
                     )[0]
-        bg_stats['GEOID'] = bg_row['GEOID']
-        census_fields.append(bg_stats)
+        census_fields[ bg_row['GEOID'] ] = bg_stats
         i = i + 1
 
     return census_fields
-
-# TODO
-# def dem_lean_by_age():
-
 
 
 def to_json(data, out_filename='static/data/out.json'):
@@ -190,12 +185,13 @@ def to_json(data, out_filename='static/data/out.json'):
         json.dump(data, outfile)
 
 
-def get_census_fields(table, year=2015):
+def get_census_fields_by_table(table, year=2015):
     """Return the fields in a census table
     Args: 
         table: 
         year:  
     Returns: 
+        TODO document structure of fields and labels
         fields: 
         labels: 
     Raises:
@@ -221,22 +217,194 @@ def get_census_fields(table, year=2015):
     return fields, labels
 
 
+def make_age_data(api, state=48, district=7, year=2015, make_blockgroup=True, 
+        make_tract=False, make_voting_precinct=False):
+    """Return the fields in a census table
+    Args: 
+        api: 
+        state: 
+        district: 
+        year: 
+        make_blockgroup: dtype(bool)
+        make_tract: dtype(bool)
+        make_voting_precinct: dtype(bool)
+    Returns: 
+        data:
+        fields: 
+        labels: 
+    Raises:
+        Nothing (yet)
+
+    PID by Age Class:
+                                R       D       I       O       LR      LD      NL      UN
+                                %	%	%	%	%	%	%	
+    DETAILED GENERATION
+    Younger Millennial (18-25)	22	33	43	2	36	58	6	613
+    Older Millennial (26-35)	22	35	39	4	36	56	8	982
+    Younger Gen Xer (36-43)	24	34	37	4	38	51	11	809
+    Older Gen Xer (44-51)	30	32	35	3	46	46	8	1,055
+    Younger Boomer (52-60)	32	33	32	3	48	46	6	1,602
+    Older Boomer (61-70)	33	34	28	4	49	44	7	1,711
+    Younger Silent (71-80)	39	33	24	4	53	41	7	928
+    Older Silent (81-88)	43	31	19	8	53	39	8	248
+        Rep = R
+        Dem = D	
+        Ind = I	
+        Other = O
+        Lean Rep = LR	
+        Lean Dem = LD
+        No leaning  = NL
+        Unweighted N = UN
+    
+    See http://www.people-press.org/2016/09/13/2016-party-identification-detailed-tables/
+
+    """
+    
+    total_field = 'B01001_001E'
+    over_18_field = 'over_18'
+
+    under_18_fields = [
+                'B01001_003E', # Male:!!Under 5 years
+                'B01001_004E', # Male:!!5 to 9 years
+                'B01001_005E', # Male:!!10 to 14 years
+                'B01001_006E', # Male:!!15 to 17 years
+                'B01001_027E', # Female:!!Under 5 years
+                'B01001_028E', # Female:!!5 to 9 years
+                'B01001_029E', # Female:!!10 to 14 years
+                'B01001_030E'  # Female:!!15 to 17 years
+            ]
+    pid_18_to_25_fields = [ 
+                'B01001_007E', # Male:!!18 and 19 years
+                'B01001_008E', # Male:!!20 years
+                'B01001_009E', # Male:!!21 years
+                'B01001_010E', # Male:!!22 to 24 years
+                'B01001_031E', # Female:!!18 and 19 years
+                'B01001_032E', # Female:!!20 years
+                'B01001_033E', # Female:!!21 years
+                'B01001_034E'  # Female:!!22 to 24 years
+            ]
+    pid_26_to_35_fields = [
+                'B01001_011E', # Male:!!25 to 29 years
+                'B01001_012E', # Male:!!30 to 34 years
+                'B01001_035E', # Female:!!25 to 29 years
+                'B01001_036E'  # Female:!!30 to 34 years
+            ]
+    pid_36_to_43_fields = [
+                'B01001_013E', # Male:!!35 to 39 years
+                'B01001_014E', # Male:!!40 to 44 years
+                'B01001_037E', # Female:!!35 to 39 years
+                'B01001_038E'  # Female:!!40 to 44 years
+            ]
+    pid_44_to_51_fields = [
+                'B01001_015E', # Male:!!45 to 49 years
+                'B01001_039E'  # Female:!!45 to 49 years
+            ]
+    pid_52_to_60_fields = [
+                'B01001_016E', # Male:!!50 to 54 years
+                'B01001_017E', # Male:!!55 to 59 years
+                'B01001_040E', # Female:!!50 to 54 years
+                'B01001_041E', # Female:!!55 to 59 years
+            ]
+    pid_61_to_70_fields = [
+                'B01001_018E', # Male:!!60 and 61 years
+                'B01001_019E', # Male:!!62 to 64 years
+                'B01001_020E', # Male:!!65 and 66 years
+                'B01001_021E', # Male:!!67 to 69 years
+                'B01001_042E', # Female:!!60 and 61 years
+                'B01001_043E', # Female:!!62 to 64 years
+                'B01001_044E', # Female:!!65 and 66 years
+                'B01001_045E', # Female:!!67 to 69 years
+            ]
+    pid_71_to_80_fields = [
+                'B01001_022E', # Male:!!70 to 74 years
+                'B01001_023E', # Male:!!75 to 79 years
+                'B01001_046E', # Female:!!70 to 74 years
+                'B01001_047E', # Female:!!75 to 79 years
+            ]
+    pid_81_plus_fields = [
+                'B01001_024E', # Male:!!80 to 84 years
+                'B01001_025E', # Male:!!85 years and over
+                'B01001_048E', # Female:!!80 to 84 years
+                'B01001_049E', # Female:!!85 years and over
+                
+            ]
+    
+    age_dict = {
+                total_field: { 'label': 'Total', 'fields': [total_field] },
+                over_18_field: { 'label': 'Over 18', 'fields': under_18_fields},
+                'pid_18_to_25': { 'label': 'PID 18-25', 'fields': pid_18_to_25_fields, 'pid': 0.58},
+                'pid_26_to_35': { 'label': 'PID 26-35', 'fields': pid_26_to_35_fields, 'pid': 0.56},
+                'pid_36_to_43': { 'label': 'PID 36-43', 'fields': pid_36_to_43_fields, 'pid': 0.51}, 
+                'pid_44_to_51': { 'label': 'PID 44-51', 'fields': pid_44_to_51_fields, 'pid': 0.46},
+                'pid_52_to_60': { 'label': 'PID 52-60', 'fields': pid_52_to_60_fields, 'pid': 0.46},
+                'pid_61_to_70': { 'label': 'PID 61-70', 'fields': pid_61_to_70_fields, 'pid': 0.44},
+                'pid_71_to_80': { 'label': 'PID 71-80', 'fields': pid_71_to_80_fields, 'pid': 0.41},
+                'pid_81_plus':  { 'label': 'PID 81+', 'fields': pid_81_plus_fields, 'pid': 0.39},
+            }
+    
+    data_in_bgs = {}
+    
+    print "Getting Census Data for Sex by Age"
+    census_fields, census_labels = get_census_fields_by_table("B01001")
+    census_data_in_bgs = get_blockgroup_census_data(api=api, fields=census_fields, year=year)
+
+    fields = []
+    labels = {}
+    
+    for age_field, age_row in age_dict.iteritems():
+        fields.append(age_field)
+        labels[age_field] = age_row['label']
+
+    fields.append('pid_total')
+    labels['pid_total']='PID Total'
+
+    print "\nCalculating PID by Age"
+    for census_key, census_row in census_data_in_bgs.iteritems():
+        pid_total = 0.0
+        pid_class = 0.0
+        data_in_bgs[census_key] = {}
+        for age_key, age_row in age_dict.iteritems():
+            if age_key is total_field:
+                # Total
+                data_in_bgs[census_key][age_key] = census_row[total_field]
+            elif age_key is 'over_18':
+                under_18 = 0
+                for census_field in age_row['fields']:
+                    under_18 = under_18 + int(census_row[census_field])
+                # (over 18) = total - (under 18)
+                data_in_bgs[census_key][age_key] = int(census_row[total_field]) - under_18
+            else:
+                for census_field in age_row['fields']:
+                    pid_class =  float(census_row[census_field]) * float(age_row['pid'])
+                    pid_total = pid_total + pid_class
+                
+                data_in_bgs[census_key][age_key] = int(pid_class)
+
+        # PID Total
+        data_in_bgs[census_key]['pid_total'] = int(pid_total)
+        
+
+        # TODO
+        # Max PID Class
+    
+    return fields, labels, data_in_bgs
+
+
 def main():
     """Builds stats and progressive scores for a Congressional District.
     """
     args = get_command_line_args()
     settings_dict = read_settings(args)
     census_api_key = settings_dict['census_api_key']
+    state = 48
+    district = 7
     year = 2015
     
-    fields, labels = get_census_fields("B01001")
-
-    print "Getting Sex by Age stats for TX-07"
-    data_in_bgs = get_blockgroup_data(api=census_api_key, fields=fields, year=year)
+    fields, labels, data_in_bgs = make_age_data(census_api_key)
     
-    to_json(data_in_bgs, "static/data/sex_by_age_in_tx7.json")
-
-    to_json([fields, labels], "static/data/labels.json")
+    to_json(data_in_bgs, "static/data/pid_by_age.json")
+    to_json(fields, "static/data/fields.json")
+    to_json(labels, "static/data/labels.json")
 
 
 if __name__ == "__main__":
--- static/js/script.js
+++ static/js/script.js
@@ -9,6 +9,15 @@
  *
  */
 
+// Global Variables
+var map;
+var variables = [];
+var labels;
+var ranges = {};
+var district_layer;
+var geounits_layer;
+var district_min = Number.MAX_VALUE; 
+var district_max = -Number.MAX_VALUE;
 
 function loadGoogleMapsAPI() {
     var script = document.createElement('script');
@@ -20,17 +29,23 @@ function loadGoogleMapsAPI() {
 }
 
 window.onload = loadGoogleMapsAPI;
-
+ 
 function initMap() {
+	/* TODO
+	 * get variable settings for a particular congressional district 
+	 */
+	
 	var uluru = {lat: 29.8, lng: -95.6};
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
     	zoom: 11,
     	center: uluru,
     	fullscreenControl: true
     });
 
-	var district_layer = new google.maps.Data({map: map});
-	var geounits_layer = new google.maps.Data({map: map});
+	// Create data layers for Congressional District and
+	// the sub-geounits, e.g., voting precinct, tract, or block group
+	district_layer = new google.maps.Data({map: map});
+	geounits_layer = new google.maps.Data({map: map});
 	
 	district_layer.loadGeoJson("/static/geojson/tx7.geojson");
 	geounits_layer.loadGeoJson("/static/geojson/tx7-blockgroups.geojson");
@@ -46,9 +61,116 @@ function initMap() {
 	
 	geounits_layer.addListener('mouseover', mouseInToRegion);
     geounits_layer.addListener('mouseout', mouseOutOfRegion);
+
+	// fill in the options for the variable selection
+	$.ajax({
+  		url: '/static/data/fields.json',
+  		async: false,
+  		dataType: 'json',
+  		success: function (json) {
+    		variables = json;
+  		}
+	});	
+	$.ajax({
+  		url: '/static/data/labels.json',
+  		async: false,
+  		dataType: 'json',
+  		success: function (json) {
+    		labels = json;
+  		}
+	});
+	console.log(variables);
+
+	var selectBox = document.getElementById('variables');
+	for (var i = 0; i < variables.length; i++) {
+		ranges[variables[i]] = { min: Infinity, max: -Infinity };
+		// Simultaneously, build the UI for selecting different
+		// ranges
+			$('<option></option>')
+					.text(labels[variables[i]])
+			.attr('value', variables[i])
+			.appendTo(selectBox);
+	}
+
+	google.maps.event.addDomListener(selectBox, 'change', function() {
+          clearData();
+          loadData(selectBox.options[selectBox.selectedIndex].value);
+    });
+}
+
+function loadData(selected_variable) {
+	data = {};
+	$.ajax({
+  		url: '/static/data/pid_by_age.json',
+  		async: false,
+  		dataType: 'json',
+  		success: function (json) {
+    		data = json;
+  		}
+	});	
+	
+	console.log(data);
+	geounits_layer.forEach(function(feature){
+		var geoid = feature.getProperty('GEOID');
+		var geo_variable = parseInt(data[geoid.toString()][selected_variable]);
+
+		console.log(geo_variable);
+		console.log(typeof(geo_variable));
+
+		// keep track of min and max values
+		if (geo_variable < district_min) {
+		  	district_min = geo_variable;
+		}
+		if (geo_variable > district_max) {
+		  	district_max = geo_variable;
+		}
+
+		// update the existing row with the new data
+		feature.setProperty('geo_variable', geo_variable);
+
+	});
+
+	// update and display the legend
+	document.getElementById('variable-min').textContent =
+	  district_min.toString();
+	document.getElementById('variable-max').textContent =
+	  district_max.toString();
+
+}
+
+
+/** Removes census data from each shape on the map and resets the UI. */
+function clearData() {
+	district_min = Number.MAX_VALUE;
+	district_max = -Number.MAX_VALUE;
+	geounits_layer.forEach(function(row) {
+	  row.setProperty('geo_variable', undefined);
+	});
+	document.getElementById('data-box').style.display = 'none';
+	document.getElementById('data-caret').style.display = 'none';
 }
 
 function styleFeature(feature) {
+	var low = [5, 69, 54];  // color of smallest datum
+	var high = [151, 83, 34];   // color of largest datum
+
+	// delta represents where the value sits between the min and max
+	var delta = (feature.getProperty('geo_variable') - district_min) /
+		(district_max - district_min);
+
+	var color = [];
+	for (var i = 0; i < 3; i++) {
+	  // calculate an integer color based on the delta
+	  color[i] = (high[i] - low[i]) * delta + low[i];
+	}
+
+	// determine whether to show this shape or not
+	var show_row = true;
+	if (feature.getProperty('geo_variable') == null || 
+			isNaN(feature.getProperty('geo_variable'))) {
+		show_row = false;
+	}
+
 	var outlineWeight = 0.5, zIndex = 1;
 	if (feature.getProperty('state') === 'hover') {
 		outlineWeight = 3;
@@ -56,17 +178,20 @@ function styleFeature(feature) {
 	}
 
 	return {
-	  strokeWeight: outlineWeight,
-	  strokeColor: '#fff',
-	  zIndex: zIndex,
-	  fillColor: '#2171b5',
-	  fillOpacity: 0.75
+	  	strokeWeight: outlineWeight,
+	  	strokeColor: '#fff',
+	  	zIndex: zIndex,
+	  	fillColor: 'hsl(' + color[0] + ',' + color[1] + '%,' + color[2] + '%)',
+	  	fillOpacity: 0.75,
+		visible: show_row
 	};
 }
 
 function mouseInToRegion(e) {
 	// set the hover state so the setStyle function can change the border
 	e.feature.setProperty('state', 'hover');
+ 	console.log('GEOID: ' + e.feature.getProperty('GEOID'));
+ 	console.log(e.feature.getProperty('geo_variable'));
 }
 
 function mouseOutOfRegion(e) {
