0515b3b1c2906c8a8cd4f579ace71f7f3648452c
James Sinton <jksinton@gmail.com>
2018-02-07 21:14:06 -0600

    bumping copyright

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 /*
  * This file is part of Statistical Districts.
  * 
- * Copyright (c) 2017, James Sinton
+ * Copyright (c) 2018, James Sinton
  * All rights reserved.
  * 
  * Released under the BSD 3-Clause License
